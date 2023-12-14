2793613ff9c003aff6db8bc3651ab694688f03dc
Mikhail Sulim <mika-1984@yandex.ru>
2023-12-14 19:52:01 +0300

    feat: add slider block

---
+++ src/components/Main/Gallery/Gallery.tsx
@@ -0,0 +1,105 @@
+import Icon from '../../Icon/Icon';
+import './Gallery.scss';
+
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+
+export const Gallery: React.FC = () => {
+  const [images, setImages] = useState<string[]>([]);
+  const [currentImage, setCurrentImage] = useState<number>(0);
+
+  useEffect(() => {
+    const context = (require as any).context(
+      './../../../images/slides',
+      false,
+      /\.(png|jpe?g|svg)$/
+    );
+    const allImages: string[] = context
+      .keys()
+      .map((key: string) => context(key));
+    setImages(allImages);
+    // console.log(allImages, images);
+  }, []);
+
+  const handlePrev = () => {
+    if (currentImage > 0) {
+      setCurrentImage((prevImage) => prevImage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentImage < images.length - 1) {
+      setCurrentImage((prevImage) => prevImage + 1);
+    }
+  };
+
+  const sliderRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (sliderRef.current)
+      console.log(
+        (sliderRef.current.style.transform = `translateX(${
+          -100 * currentImage
+        }%)`)
+      );
+  }, [currentImage]);
+
+  const galleryPagination = useMemo(
+    () => (
+      <div className="gallery__slider">
+        <div ref={sliderRef} className="gallery__slider-container">
+          {images.map((item) => (
+            <img
+              className="gallery__slider-image"
+              key={item}
+              src={item}
+              alt=""
+            />
+          ))}
+        </div>
+
+        <div className="gallery__pagination">
+          {images.map((item, idx) => (
+            <div
+              key={`pagination_${item}`}
+              className={`gallery__pagination-item ${
+                idx === currentImage ? 'gallery__pagination-item_current' : ''
+              }`}
+              onClick={() => {
+                setCurrentImage(idx);
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    ),
+    [currentImage, images]
+  );
+
+  return (
+    <section className="gallery">
+      <div className="gallery__header">
+        <h2 className="gallery__title">Корпоративная жизнь</h2>
+        <div className="gallery__navigation">
+          <button
+            className="gallery__navigation-btn gallery__navigation-btn_left"
+            type="button"
+            onClick={handlePrev}
+            disabled={currentImage === 0}
+          >
+            <Icon iconId="arrRight" />
+          </button>
+          <button
+            className="gallery__navigation-btn "
+            type="button"
+            onClick={handleNext}
+            disabled={currentImage === images.length - 1}
+          >
+            <Icon iconId="arrRight" />
+          </button>
+        </div>
+      </div>
+
+      {galleryPagination}
+    </section>
+  );
+};
